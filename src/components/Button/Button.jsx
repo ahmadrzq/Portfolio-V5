9262a5dd2e88515d3f4b779d034662fc78e39572
ahmadrzq <ahmadrizqiisnain@gmail.com>
2023-10-21 18:53:43 +0700

    Create button component and styles

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,7 +1,10 @@
+import { Link } from "react-router-dom"
 import "./Button.css"
 
-export default function Button() {
+export default function Button({ to, value }) {
   return (
-    <div>Button</div>
+    <Link to={to} className="btn__default-light"
+    >{value}
+    </Link>
   )
 }
