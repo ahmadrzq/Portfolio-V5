252a2ac3bf7c8d84a33f25e3bf6e84b9a43cc13b
ahmadrzq <ahmadrizqiisnain@gmail.com>
2023-10-21 21:21:15 +0700

    Create education & experience pages and styles

--- src/pages/Education/Education.jsx
+++ src/pages/Education/Education.jsx
@@ -1,7 +1,30 @@
+import CardBox from '../../components/Card/Box/CardBox'
+import Title from '../../components/Title/Title'
 import './Education.css'
 
 export default function Education() {
+  const educations = [
+    {
+      name: 'University Jember',
+      major: 'Information Technology',
+      years: '2018-2023'
+    },
+    {
+      name: 'SMAN 3 Probolinggo',
+      major: 'Science',
+      years: '2016-2018'
+    }
+  ]
   return (
-    <div>Education</div>
+    <section className="education section">
+      <Title value="My Education" />
+      <div className="education__content">
+        {
+          educations.map((education, index) => (
+            <CardBox key={index} name={education.name} major={education.major} years={education.years} />
+          ))
+        }
+      </div>
+    </section>
   )
 }
--- src/pages/Experience/Experience.jsx
+++ src/pages/Experience/Experience.jsx
@@ -1,7 +1,29 @@
+import CardBox from '../../components/Card/Box/CardBox'
+import Title from '../../components/Title/Title'
 import './Experience.css'
 
 export default function Experience() {
+  const experiences = [
+    {
+      name: 'Freelance Web Developer',
+      company: 'Work From Anywhere',
+      years: '2023 - Now'
+    },
+    {
+      name: 'Project-Based Intern : Fullstack Developer Virtual Internship Experience Investree',
+      company: 'Investree',
+      years: 'June 2022 - July 2022'
+    }
+  ]
   return (
-    <div>Experience</div>
+    <section className="experience section">
+      <Title value="My Experience" />
+      <div className="experience__content">
+        {
+          experiences.map((experience, index) => (
+            <CardBox key={index} name={experience.name} company={experience.company} years={experience.years} />))
+        }
+      </div>
+    </section >
   )
 }
