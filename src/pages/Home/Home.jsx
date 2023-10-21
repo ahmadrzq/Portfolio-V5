3c3702fb96c7eb9f71cafc0c75c3e3cc32ea617f
ahmadrzq <ahmadrizqiisnain@gmail.com>
2023-10-21 18:54:39 +0700

    Create home pages and styles

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,26 @@
+import Button from '../../components/Button/Button'
 import './Home.css'
+import me from '/image/me.png'
 
 export default function Home() {
   return (
-    <div>Home</div>
+    <section className="homepage">
+      <div className="home__img">
+        <img className="home__img-img" src={me} alt="" />
+      </div>
+      <div className="home__content">
+        <h2>Welcome to my Website</h2>
+        <h1>
+          I'm <span className="home__content-name">Ahmad Rizqi Isnain</span>
+        </h1>
+        <h4>
+          A <span className="home__content-name">Web Developer</span>, Building
+          Your Web Dreams.
+        </h4>
+      </div>
+      <div className="home__btn">
+        <Button to={"/portfolio"} value={"My Portfolio"}/>
+      </div>
+    </section>
   )
 }
