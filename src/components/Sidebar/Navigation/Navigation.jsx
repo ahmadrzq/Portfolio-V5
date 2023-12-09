d55a27c1f2b450555e840b1fa898169364537597
ahmadrzq <ahmadrizqiisnain@gmail.com>
2023-12-09 20:13:14 +0700

    Remove link to blog page in navigation

--- src/components/Sidebar/Navigation/Navigation.jsx
+++ src/components/Sidebar/Navigation/Navigation.jsx
@@ -7,7 +7,7 @@ export default function Navigation() {
             <ul className="nav__list">
                 <li><Link to="/">Home</Link></li>
                 <li><Link to="/about">About</Link></li>
-                <li><Link to="/blog">Blog</Link></li>
+                {/* <li><Link to="/blog">Blog</Link></li> */}
                 <li><Link to="/contact">Contact</Link></li>
                 <li><Link to="/education">Education</Link></li>
                 <li><Link to="/experience">Experience</Link></li>
